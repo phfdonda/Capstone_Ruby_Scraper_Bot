c7665f04ace151d8e11af8caead688a83c0e2fec
Pedro Donda <phfdonda@gmail.com>
2020-04-29 17:16:32 -0300

    Added "100 best" functionality to algorithm

--- main.rb
+++ main.rb
@@ -9,19 +9,24 @@ def scraper
   unparsed_page = HTTParty.get(url)
   parsed_page = Nokogiri::HTML(unparsed_page)
   articles = parsed_page.css('li.mdl')
-  articles.each do |article|
-    review = {
-      title: article.css('a.meta-title-link').text,
-      release: article.css('div.meta-body-info').text.gsub("\n", '').gsub('  ', '').gsub("Desde ", "").split('/')[0],
-      duration: article.css('div.meta-body-info').text.gsub("\n", '').gsub('  ', '').split('/')[1],
-      genre: article.css('div.meta-body-info').text.gsub("\n", '').gsub('  ', '').split('/')[2],
-      director: article.css('div.meta-body-direction').text.gsub('  ', '').gsub("\n", '').gsub('Direção:', ''),
-      cast: article.css('div.meta-body-actor').text.gsub('  ', '').gsub("\n", '').gsub('Elenco:', ''),
-      review_text: article.css('div.content-txt').text.gsub('  ', '').gsub("\n", ''),
-      stars: article.css('span.stareval-note').text.gsub("--","").gsub(",",".").to_f
-    }
-    byebug
+  review_count = 0
+  reviews = Array.new
+  while review_count < 100
+    articles.each do |article|
+      review = {
+        title: article.css('a.meta-title-link').text,
+        release: article.css('div.meta-body-info').text.gsub("\n", '').gsub('  ', '').gsub("Desde ", "").split('/')[0],
+        duration: article.css('div.meta-body-info').text.gsub("\n", '').gsub('  ', '').split('/')[1],
+        genre: article.css('div.meta-body-info').text.gsub("\n", '').gsub('  ', '').split('/')[2],
+        director: article.css('div.meta-body-direction').text.gsub('  ', '').gsub("\n", '').gsub('Direção:', ''),
+        cast: article.css('div.meta-body-actor').text.gsub('  ', '').gsub("\n", '').gsub('Elenco:', ''),
+        review_text: article.css('div.content-txt').text.gsub('  ', '').gsub("\n", ''),
+        stars: article.css('span.stareval-note').text.gsub("--","").gsub(",",".").to_f
+      }
+      reviews << review && review_count += 1 if review[:stars] > 4.3
+    end
   end
+  byebug
 end
 
 scraper
